3ce521676410a32024274fb25415c6ec5863382d
XenonShawn <57314121+XenonShawn@users.noreply.github.com>
2021-03-16 09:08:34 +0800

    Create faktor.cpp

---
+++ faktor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    unsigned A, I;
+    cin >> A >> I;
+    cout << A * (I - 1) + 1 << endl;
+}  
