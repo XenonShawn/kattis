74a462570840d6e4670f59132f4129ecf63e7d00
XenonShawn <57314121+XenonShawn@users.noreply.github.com>
2021-05-20 13:36:26 +0800

    Create 4thought.cpp
    Used a python script to generate all possibilities and the switch case code.

---
+++ 4thought.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    
+    for (int i = 0; i < N; i++) {
+        int p;
+        cin >> p;
+        switch (p) {
+case -60:
+cout << "4 - 4 * 4 * 4 = -60\n";
+break;
+case -16:
+cout << "4 - 4 * 4 - 4 = -16\n";
+break;
+case -15:
+cout << "4 / 4 - 4 * 4 = -15\n";
+break;
+case -8:
+cout << "4 - 4 * 4 + 4 = -8\n";
+break;
+case -7:
+cout << "4 / 4 - 4 - 4 = -7\n";
+break;
+case -4:
+cout << "4 / 4 / 4 - 4 = -4\n";
+break;
+case -1:
+cout << "4 - 4 / 4 - 4 = -1\n";
+break;
+case 0:
+cout << "4 / 4 / 4 / 4 = 0\n";
+break;
+case 1:
+cout << "4 / 4 * 4 / 4 = 1\n";
+break;
+case 2:
+cout << "4 / 4 + 4 / 4 = 2\n";
+break;
+case 4:
+cout << "4 / 4 / 4 + 4 = 4\n";
+break;
+case 7:
+cout << "4 - 4 / 4 + 4 = 7\n";
+break;
+case 8:
+cout << "4 / 4 * 4 + 4 = 8\n";
+break;
+case 9:
+cout << "4 / 4 + 4 + 4 = 9\n";
+break;
+case 15:
+cout << "4 * 4 - 4 / 4 = 15\n";
+break;
+case 16:
+cout << "4 / 4 * 4 * 4 = 16\n";
+break;
+case 17:
+cout << "4 / 4 + 4 * 4 = 17\n";
+break;
+case 24:
+cout << "4 * 4 + 4 + 4 = 24\n";
+break;
+case 32:
+cout << "4 * 4 + 4 * 4 = 32\n";
+break;
+case 60:
+cout << "4 * 4 * 4 - 4 = 60\n";
+break;
+case 68:
+cout << "4 * 4 * 4 + 4 = 68\n";
+break;
+case 256:
+cout << "4 * 4 * 4 * 4 = 256\n";
+break;
+default:
+cout << "no solution\n";
+        }
+    }
+}
